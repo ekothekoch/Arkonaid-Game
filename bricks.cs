576467ce9543fe99e04f3a5f6fcd521f19baead8
ekothekoch <ekothekoch@gmail.com>
2020-02-13 14:14:43 +0300

    Add files via upload
    Codes for the arkonoid are ready here

---
+++ bricks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bricks : MonoBehaviour {
+
+	public GameObject brickParticle;
+
+	void OnCollisionEnter(Collision other)
+	{
+		Instantiate(brickParticle, transform.position, Quaternion.identity);
+		GM.instance.DestroyBrick();
+		Destroy(gameObject);
+	}
+}
